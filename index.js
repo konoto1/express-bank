c580288dd97d38d961bd1da7a71d5b144b191166
Tomas.K <tomas.konopackis@gmail.com>
2024-08-28 17:14:29 +0300

    updated account create

--- index.js
+++ index.js
@@ -8,16 +8,27 @@ app.use(express.json({
     type: 'application/json',
 }));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
     return res.send('Bank');
-})
+});
 
 app.get('*', (req, res) => {
     return res.send('Ups 404 puslpais nerastas....👽');
-})
+});
+
+app.use((req, res, next) => {
+    return res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
-})
+});
--- router/apiRouter.js
+++ router/apiRouter.js
@@ -14,22 +14,63 @@ apiRouter.get('/', (req, res) => {
 
 
 apiRouter.post('/account', (req, res) => {
-    const test = {
-        jonasJonaitis: {
-            name: 'Jonas',
-            surname: 'Jonaitis',
-            dob: '1980-10-02',
-        }
-    };
-    console.log(JSON.stringify(test));
+    const name = req.body.nameSurname.split('-')[0];
+    const surname = req.body.nameSurname.split('-')[1];
+    const dob = parseInt(req.body.DOB.replaceAll('-', ''));
+    const date = new Date();
+    const year = '' + date.getFullYear();
+    const month = ('' + (date.getMonth() + 1)).padStart(2, 0);
+    const day = ('' + (date.getDate())).padStart(2, 0);
+    const minDate = parseInt(`${year - 18}${month}${day}`);
 
-    accounts.push(req.body);
-    return res.json(accounts);
+    if (name[0] !== name[0].toUpperCase()) {
+        const data = {
+            state: 'error',
+            message: 'Vardas privalo prasideti didziaja raide',
+        };
+        return res.json(data);
+    };
+
+    if (surname[0] !== surname[0].toUpperCase()) {
+        const data = {
+            state: 'error',
+            message: 'Pavarde privalo prasideti didziaja raide',
+        };
+        return res.json(data);
+    };
+
+    if (dob > minDate) {
+        const data = {
+            state: 'error',
+            message: 'Saskaita susikurti gali asmenys ne jaunesni, nei 18m amziaus',
+        };
+
+        return res.json(data);
+
+    };
+
+    const data = {
+        state: 'success',
+        message: 'Saskaita sekmingai sukurta',
+    };
+
+    return res.json(data);
+
 });
 
-apiRouter.get('/account', (req, res) => {
+apiRouter.get('/account/:nameSurname', (req, res) => {
+    const { nameSurname } = req.params;
+    const inputName = nameSurname.split('-')[0].toLowerCase();
+    const inputSurname = nameSurname.split('-')[1].toLowerCase();
+    const inputNameSurname = nameSurname.toLowerCase();
+    if (accounts.length === 0) {
+        return res.json(`Saskaitu sarasas tuscias.`);
+    }
+
+    console.log(accounts.map((item, index) => item.nameSurname));
+    console.log(inputNameSurname);
 
 
-    // accounts.push('vardas pavarde gimimo data');
-    return res.json(accounts);
+
+    return res.send([inputName, inputSurname]);
 });
