2c30164e21f269060d259e6803db655098593bc5
Tomas.K <tomas.konopackis@gmail.com>
2024-08-29 09:44:31 +0300

    updated create account method with validations

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -3,6 +3,8 @@ import { accounts } from "../data/accounts.js";
 
 export const apiRouter = express.Router();
 
+const validSymblos = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+
 apiRouter.get('/', (req, res) => {
     const data = {
         state: 'error',
@@ -14,14 +16,56 @@ apiRouter.get('/', (req, res) => {
 
 
 apiRouter.post('/account', (req, res) => {
-    const name = req.body.nameSurname.split('-')[0];
-    const surname = req.body.nameSurname.split('-')[1];
-    const dob = parseInt(req.body.DOB.replaceAll('-', ''));
+
+    if (Object.keys(req.body)[0] !== 'nameSurname' || Object.keys(req.body)[1] !== 'DOB') {
+        const data = {
+            state: 'error',
+            message: 'Panaudoti netinkami Objekto raktai. Galimi raktai `nameSurname`, `DOB`.',
+        };
+        return res.json(data);
+    };
+
+    if (Object.keys(req.body).length !== 2) {
+        const data = {
+            state: 'error',
+            message: 'Objektas gali tureti tik du raktus.',
+        };
+        return res.json(data);
+    }
+
+    const { nameSurname, DOB } = req.body;
+    const name = nameSurname.split('-')[0];
+    const surname = nameSurname.split('-')[1];
+    const dob = parseInt(DOB.replaceAll('-', ''));
     const date = new Date();
     const year = '' + date.getFullYear();
     const month = ('' + (date.getMonth() + 1)).padStart(2, 0);
     const day = ('' + (date.getDate())).padStart(2, 0);
     const minDate = parseInt(`${year - 18}${month}${day}`);
+
+    if (nameSurname.startsWith('-') || nameSurname.endsWith('-')) {
+        const data = {
+            state: 'error',
+            message: 'Vardas ir pavarde negali prasideti ar baigtis `-` simboliu.',
+        };
+        return res.json(data);
+    };
+
+    if (!nameSurname.includes('-')) {
+        const data = {
+            state: 'error',
+            message: 'Vardo ir pavarde turi but atskirti `-` simboliu.',
+        };
+        return res.json(data);
+    };
+
+    if (nameSurname.trim() !== nameSurname) {
+        const data = {
+            state: 'error',
+            message: 'Vardo ir pavardes ivestyje negali buti tusciu tarpu',
+        };
+        return res.json(data);
+    };
 
     if (name[0] !== name[0].toUpperCase()) {
         const data = {
@@ -39,20 +83,47 @@ apiRouter.post('/account', (req, res) => {
         return res.json(data);
     };
 
+    let count = 0;
+    for (const symbol of nameSurname) {
+
+        if (symbol === '-') {
+            count++;
+        };
+
+        if (count > 1) {
+            const data = {
+                state: 'error',
+                message: 'Vardo ir pavardes ivestyje ` - ` simbolis gali buti panaudotas tik viena karta.',
+            };
+            count = 0;
+            return res.json(data);
+        };
+
+        if (!validSymblos.includes(symbol) && symbol !== '-') {
+            const data = {
+                state: 'error',
+                message: `Vardo ir pavardes ivestis turi neleistinu simboliu. Lesitini simboliai: (${validSymblos}) `,
+            };
+            count = 0;
+            return res.json(data);
+        };
+    };
+
     if (dob > minDate) {
         const data = {
             state: 'error',
             message: 'Saskaita susikurti gali asmenys ne jaunesni, nei 18m amziaus',
         };
-
         return res.json(data);
-
     };
 
     const data = {
         state: 'success',
-        message: 'Saskaita sekmingai sukurta',
+        message: 'Saskaita sekmingai sukurta.',
     };
+
+    accounts.push({ ...req.body, "money": "0" });
+    console.log(accounts);
 
     return res.json(data);
 
@@ -67,8 +138,8 @@ apiRouter.get('/account/:nameSurname', (req, res) => {
         return res.json(`Saskaitu sarasas tuscias.`);
     }
 
-    console.log(accounts.map((item, index) => item.nameSurname));
-    console.log(inputNameSurname);
+    // console.log(accounts.map((item, index) => item.nameSurname));
+    // console.log(inputNameSurname);
 
 
 
