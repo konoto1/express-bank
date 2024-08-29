12df22a7005fe0da3ee2637d073da9b0c294fcd1
Tomas.K <tomas.konopackis@gmail.com>
2024-08-29 17:04:46 +0300

    update with date validation

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { accounts } from "../data/accounts.js";
+import { inputCheck } from "../lib/helpers.js";
 
 export const apiRouter = express.Router();
 
-const validSymblos = 'qwertyuiopasdfghjklzxcvbnm-QWERTYUIOPASDFGHJKLZXCVBNM';
+
 
 apiRouter.get('/', (req, res) => {
     const data = {
@@ -13,120 +14,12 @@ apiRouter.get('/', (req, res) => {
     return res.json(data.message);
 });
 
-apiRouter.post('/account', (req, res) => {
+apiRouter.post('/account', inputCheck);
 
-    if (Object.keys(req.body)[0] !== 'nameSurname' || Object.keys(req.body)[1] !== 'DOB') {
-        const data = {
-            state: 'error',
-            message: 'Panaudoti netinkami Objekto raktai. Galimi raktai `nameSurname`, `DOB`.',
-        };
-        return res.json(data);
-    };
 
-    if (Object.keys(req.body).length !== 2) {
-        const data = {
-            state: 'error',
-            message: 'Objektas gali tureti tik du raktus.',
-        };
-        return res.json(data);
-    }
 
-    const { nameSurname, DOB } = req.body;
-    const name = nameSurname.split('-')[0];
-    const surname = nameSurname.split('-')[1];
-    const dob = parseInt(DOB.replaceAll('-', ''));
-    const date = new Date();
-    const year = '' + date.getFullYear();
-    const month = ('' + (date.getMonth() + 1)).padStart(2, 0);
-    const day = ('' + (date.getDate())).padStart(2, 0);
-    const minDate = parseInt(`${year - 18}${month}${day}`);
 
-    if (name.slice(1) !== name.slice(1).toLowerCase() || surname.slice(1) !== surname.slice(1).toLowerCase()) {
-        const data = {
-            state: 'error',
-            message: 'Tik pirmosios vardo ir pavardes raides gali buti didziosios',
-        };
-        return res.json(data);
-    };
 
-    if (!nameSurname.includes('-')) {
-        const data = {
-            state: 'error',
-            message: 'Vardo ir pavarde turi but atskirti `-` simboliu.',
-        };
-        return res.json(data);
-    };
-
-    if (name[0] !== name[0].toUpperCase()) {
-        const data = {
-            state: 'error',
-            message: 'Vardas privalo prasideti didziaja raide',
-        };
-        return res.json(data);
-    };
-
-    if (surname[0] !== surname[0].toUpperCase()) {
-        const data = {
-            state: 'error',
-            message: 'Pavarde privalo prasideti didziaja raide',
-        };
-        return res.json(data);
-    };
-
-    let count = 0;
-    for (const symbol of nameSurname) {
-
-        if (symbol === '-') {
-            count++;
-        };
-
-        if (count > 1) {
-            const data = {
-                state: 'error',
-                message: 'Vardo ir pavardes ivestyje ` - ` simbolis gali buti panaudotas tik viena karta.',
-            };
-            count = 0;
-            return res.json(data);
-        };
-
-        if (!validSymblos.includes(symbol)) {
-            const data = {
-                state: 'error',
-                message: `Vardo ir pavardes ivestis turi neleistinu simboliu. Lesitini simboliai: (${validSymblos}) `,
-            };
-            count = 0;
-            return res.json(data);
-        };
-    };
-
-    for (const account of accounts) {
-        if (account.nameSurname === nameSurname) {
-            const data = {
-                state: 'error',
-                message: `Saskaita tokiu vardu ir pavarde '${nameSurname}' jau yra uzregistruota.`,
-            };
-            return res.json(data);
-        };
-    };
-
-    if (dob > minDate) {
-        const data = {
-            state: 'error',
-            message: 'Saskaita susikurti gali asmenys ne jaunesni, nei 18m amziaus',
-        };
-        return res.json(data);
-    };
-
-    const data = {
-        state: 'success',
-        message: 'Saskaita sekmingai sukurta.',
-    };
-
-    accounts.push({ ...req.body, "money": "0" });
-
-    return res.json(data);
-
-});
 
 apiRouter.get('/account/:nameSurname', (req, res) => {
     const { nameSurname } = req.params;
