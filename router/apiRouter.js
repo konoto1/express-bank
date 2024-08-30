a40b6660eeb12a367de8f2571ccf5333db5a21ae
Tomas.K <tomas.konopackis@gmail.com>
2024-08-30 09:00:12 +0300

    updated delete account method

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { accounts } from "../data/accounts.js";
-import { inputCheck } from "../lib/helpers.js";
+import { inputCheck } from "../lib/inputcCheck.js";
+import { checkForAccount } from "../lib/checkForAccount.js";
+import { deleteAccount } from "../lib/deleteAccount.js";
 
 export const apiRouter = express.Router();
-
-
 
 apiRouter.get('/', (req, res) => {
     const data = {
@@ -16,27 +15,6 @@ apiRouter.get('/', (req, res) => {
 
 apiRouter.post('/account', inputCheck);
 
+apiRouter.get('/account/:nameSurname', checkForAccount);
 
-
-
-
-
-apiRouter.get('/account/:nameSurname', (req, res) => {
-    const { nameSurname } = req.params;
-    const inputName = nameSurname.split('-')[0].toLowerCase();
-    const validInputName = inputName[0].toUpperCase() + inputName.slice(1);
-    const inputSurname = nameSurname.split('-')[1].toLowerCase();
-    const validInputSurname = inputSurname[0].toUpperCase() + inputSurname.slice(1);
-
-    if (accounts.length === 0) {
-        return res.send('Saskaitu sarasas yra tuscias.');
-    };
-
-    for (const account of accounts) {
-        if (`${validInputName}-${validInputSurname}` === account.nameSurname) {
-            return res.send(`Saskaitos savininko vardas: ${validInputName}, pavarde: ${validInputSurname}, gimimo data: ${account.DOB}.`);
-        };
-    };
-
-    return res.send(`Saskaita tokiu vardu ir pavarde: ${validInputName} ${validInputSurname} nerasta.`);
-});
+apiRouter.delete('/account/:nameSurname', deleteAccount);
