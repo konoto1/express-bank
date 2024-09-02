f31e906ab209df1455cc30e82908cf1706b1d451
Tomas.K <tomas.konopackis@gmail.com>
2024-09-02 08:28:50 +0300

    updated with account name change router

--- router/apiAccountRouter.js
+++ router/apiAccountRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { checkForName } from "../lib/checkForName.js";
+import { changeName } from "../lib/changeName.js";
 
 export const apiAccountRouter = express.Router({ mergeParams: true });
 
 apiAccountRouter.get('/name', checkForName);
+apiAccountRouter.put('/name', changeName);
