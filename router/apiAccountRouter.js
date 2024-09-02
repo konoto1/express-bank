0d3474317ecd433262be29489da853f6fa4f6289
Tomas.K <tomas.konopackis@gmail.com>
2024-09-02 08:58:40 +0300

    updated with get dob router

--- router/apiAccountRouter.js
+++ router/apiAccountRouter.js
@@ -3,6 +3,7 @@ import { checkForName } from "../lib/checkForName.js";
 import { changeName } from "../lib/changeName.js";
 import { checkForSurname } from "../lib/checkForSurname.js";
 import { changeSurname } from "../lib/changeSurname..js";
+import { checkForDob } from "../lib/checkForDob.js";
 
 export const apiAccountRouter = express.Router({ mergeParams: true });
 
@@ -10,3 +11,4 @@ apiAccountRouter.get('/name', checkForName);
 apiAccountRouter.put('/name', changeName);
 apiAccountRouter.get('/surname', checkForSurname);
 apiAccountRouter.put('/surname', changeSurname);
+apiAccountRouter.get('/dob', checkForDob);
