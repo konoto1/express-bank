a83939acc62d004f53a4d21822c03bc0c18ffaf7
Tomas.K <tomas.konopackis@gmail.com>
2024-09-02 08:32:58 +0300

    updated with account surname get router

--- router/apiAccountRouter.js
+++ router/apiAccountRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { checkForName } from "../lib/checkForName.js";
 import { changeName } from "../lib/changeName.js";
+import { checkForSurname } from "../lib/checkForSurname.js";
 
 export const apiAccountRouter = express.Router({ mergeParams: true });
 
 apiAccountRouter.get('/name', checkForName);
 apiAccountRouter.put('/name', changeName);
+apiAccountRouter.get('/surname', checkForSurname);
