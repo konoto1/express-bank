7591a4c078ef595d5ee72e16da1b1746c7ad0a7a
Tomas.K <tomas.konopackis@gmail.com>
2024-09-02 11:43:14 +0300

    updated with money deposti and withdraw router

--- router/apiMoneyRouter.js
+++ router/apiMoneyRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { withdrawMoney } from "../lib/withdrawMoney.js";
+import { depositMoney } from "../lib/depositMoney.js";
 
 export const apiMoneytRouter = express.Router();
 
 apiMoneytRouter.post('/withdraw', withdrawMoney);
+apiMoneytRouter.post('/deposit', depositMoney);
 
 
