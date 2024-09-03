e837fea47ac73ae87f09f22ad62fb4ed2a7b3afd
Tomas.K <tomas.konopackis@gmail.com>
2024-09-03 10:35:18 +0300

    updated with moneey transfer api router

--- router/apiMoneyRouter.js
+++ router/apiMoneyRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { withdrawMoney } from "../lib/withdrawMoney.js";
 import { depositMoney } from "../lib/depositMoney.js";
+import { transferMoney } from "../lib/transferMoney.js";
 
 export const apiMoneytRouter = express.Router();
 
 apiMoneytRouter.post('/withdraw', withdrawMoney);
 apiMoneytRouter.post('/deposit', depositMoney);
+apiMoneytRouter.post('/transfer', transferMoney);
 
 
